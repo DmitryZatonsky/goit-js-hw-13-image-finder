471f9bd9d4931987ab75b3d931aa28b94ac6d875
Dmitry Zatonsky <83658538+DmitryZatonsky@users.noreply.github.com>
2021-11-10 23:50:29 +0200

    apiService

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import apiService from './js/apiService';
+// import './js/object';
+// import './templates/form.hbs';
+// import './templates/galleryList.hbs';
+// import './templates/cardImage.hbs';
+
---
+++ src/js/apiService.js
@@ -0,0 +1,16 @@
+export default function fetchImages(evt) {
+
+const BASE_URL = 'https://pixabay.com/api';
+const MY_KEY = '24281022-8ffcad6d3ff85d8030a2c3f1e';
+
+const searchQuery = evt.target.value;
+const pageNumber = 1;
+const perPage = 12;
+
+const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=${perPage}&key=${MY_KEY}`;
+
+  return fetch(url)
+    .then(responce => {
+      return responce.json();
+    })
+} 
